412f37fbf1357940d43ba4bf2d898eeb578dc912
Balaji J <j.balaji2468@gmail.com>
2026-03-27 20:52:16 +0530

    update models
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

---
+++ internal/models/place.go
@@ -0,0 +1,10 @@
+package models
+
+type Place struct {
+	ID        int64   `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name      string  `gorm:"type:varchar(100);not null" json:"name"`
+	Latitude  float64 `gorm:"type:decimal(10,8);not null" json:"latitude"`
+	Longitude float64 `gorm:"type:decimal(11,8);not null" json:"longitude"`
+	CreatedBy int64   `gorm:"not null" json:"created_by"`
+	User      User    `gorm:"foreignKey:CreatedBy;constraint:OnDelete:CASCADE"`
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,9 @@
 package models
 
 type User struct {
-	ID       int64  `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name     string `gorm:"type:varchar(100);not null" json:"name"`
-	Email    string `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
-	Password string `gorm:"type:varchar(255);not null" json:"-"`
+	ID       int64   `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name     string  `gorm:"type:varchar(100);not null" json:"name"`
+	Email    string  `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
+	Password string  `gorm:"type:varchar(255);not null" json:"-"`
+	Places   []Place `gorm:"foreignKey:CreatedBy"`
 }
