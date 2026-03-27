3ee417b175f30a704dd49faf952e3aa3b2bea6fc
Balaji J <j.balaji2468@gmail.com>
2026-03-27 22:00:42 +0530

    refactor:add constraints
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- internal/models/place.go
+++ internal/models/place.go
@@ -5,6 +5,7 @@ type Place struct {
 	Name      string  `gorm:"type:varchar(100);not null" json:"name"`
 	Latitude  float64 `gorm:"type:decimal(10,8);not null" json:"latitude"`
 	Longitude float64 `gorm:"type:decimal(11,8);not null" json:"longitude"`
-	CreatedBy int64   `gorm:"not null" json:"created_by"`
-	User      User    `gorm:"foreignKey:CreatedBy;constraint:OnDelete:CASCADE"`
+	CreatedBy int64   `gorm:"not null;index" json:"created_by"`
+
+	User      User    `gorm:"foreignKey:CreatedBy;references:ID;constraint:OnDelete:CASCADE" json:"-"`
 }
