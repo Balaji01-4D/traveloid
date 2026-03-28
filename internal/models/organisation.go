69f1183b27b59922d4a21ba94ad103b5410ab1d2
Balaji J <j.balaji2468@gmail.com>
2026-03-28 07:33:18 +0530

    add organisation model and update other models
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- internal/models/organisation.go
+++ internal/models/organisation.go
@@ -1,11 +1,11 @@
 package models
 
-type Member struct {
+type Organisation struct {
 	ID       int64  `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name     string `gorm:"type:varchar(100);not null" json:"name"`
 	Email    string `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
 	Password string `gorm:"type:varchar(255);not null" json:"-"`
 
-	OrganisationID int64        `gorm:"not null;index" json:"organisation_id"`
-	Organisation   Organisation `gorm:"foreignKey:OrganisationID;references:ID;constraint:OnDelete:CASCADE" json:"-"`
+	Members []Member `gorm:"foreignKey:OrganisationID"`
+	Places  []Place  `gorm:"foreignKey:OrganisationID"`
 }
--- internal/models/place.go
+++ internal/models/place.go
@@ -3,10 +3,10 @@ package models
 type Place struct {
 	ID        int64   `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name      string  `gorm:"type:varchar(100);not null" json:"name"`
-	ImageLink     string  `gorm:"type:varchar(255)" json:"image_link"`
+	ImageLink string  `gorm:"type:varchar(255)" json:"image_link"`
 	Latitude  float64 `gorm:"type:decimal(10,8);not null" json:"latitude"`
 	Longitude float64 `gorm:"type:decimal(11,8);not null" json:"longitude"`
-	CreatedBy int64   `gorm:"not null;index" json:"created_by"`
 
-	User User `gorm:"foreignKey:CreatedBy;references:ID;constraint:OnDelete:CASCADE" json:"-"`
+	OrganisationID int64        `gorm:"not null;index" json:"organisation_id"`
+	Organisation   Organisation `gorm:"foreignKey:OrganisationID;references:ID;constraint:OnDelete:CASCADE" json:"-"`
 }
