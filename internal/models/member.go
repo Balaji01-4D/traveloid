71845a4cce16524d812a250f456ecbf8efb4462c
Balaji J <j.balaji2468@gmail.com>
2026-03-28 11:00:14 +0530

    remove unwanted in the organisation model
    Signed-off-by: Balaji J <j.balaji2468@gmail.com>

--- internal/models/member.go
+++ internal/models/member.go
@@ -3,8 +3,8 @@ package models
 type Role string
 
 const (
-	RoleAdmin     Role = "admin"
-	RoleDeveloper Role = "developer"
+	RoleAdmin  Role = "admin"
+	RoleMember Role = "member"
 )
 
 type Member struct {
@@ -12,7 +12,7 @@ type Member struct {
 	Name     string `gorm:"type:varchar(100);not null" json:"name"`
 	Email    string `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
 	Password string `gorm:"type:varchar(255);not null" json:"-"`
-	Role     Role   `gorm:"type:role_enum;default:'developer'" json:"role"`
+	Role     Role   `gorm:"type:role_enum;default:'member'" json:"role"`
 
 	OrganisationID int64        `gorm:"not null;index" json:"organisation_id"`
 	Organisation   Organisation `gorm:"foreignKey:OrganisationID;references:ID;constraint:OnDelete:CASCADE" json:"-"`
--- internal/models/organisation.go
+++ internal/models/organisation.go
@@ -3,8 +3,6 @@ package models
 type Organisation struct {
 	ID       int64  `gorm:"primaryKey;autoIncrement" json:"id"`
 	Name     string `gorm:"type:varchar(100);not null" json:"name"`
-	Email    string `gorm:"type:varchar(100);uniqueIndex;not null" json:"email"`
-	Password string `gorm:"type:varchar(255);not null" json:"-"`
 
 	Members []Member `gorm:"foreignKey:OrganisationID"`
 	Places  []Place  `gorm:"foreignKey:OrganisationID"`
